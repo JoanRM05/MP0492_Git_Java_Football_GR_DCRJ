44510cbe794c8fdca0c1657b6206ef68c89fd41a
cassiuste <cassiustedesco1@gmail.com>
2025-09-22 15:49:13 +0200

    Added yellow and red card attributes in Player

--- src/worldcup/Player.java
+++ src/worldcup/Player.java
@@ -3,8 +3,35 @@ package worldcup;
 public class Player extends Person{
 
 	private String position;
+	private boolean yellowCard;
+	private boolean redCard;
 	
-	
+	public Player() {
+		super();
+		this.yellowCard = false;
+		this.redCard = false;
+	}
+
+	public boolean hasYellowCard() {
+		return yellowCard;
+	}
+
+
+	public void setYellowCard(boolean yellowCard) {
+		this.yellowCard = yellowCard;
+	}
+
+
+	public boolean hasRedCard() {
+		return redCard;
+	}
+
+
+	public void setRedCard(boolean redCard) {
+		this.redCard = redCard;
+	}
+
+
 	public String getPosition() {
 		return position;
 	}

94020b0fb77f9ba0576a110d8c7f9ecafccec61a
Takkun <rodrimd231@gmail.com>
2025-09-22 16:20:34 +0200

    first arbitrate logic

--- src/worldcup/Referee.java
+++ src/worldcup/Referee.java
@@ -1,23 +1,28 @@
 package worldcup;
 
 public class Referee extends Person {
-	
-    private Player player;
 
-	
- public Player getPlayer() {
+	private Player player;
+
+	public Player getPlayer() {
 		return player;
 	}
 
-
- public void setPlayer(Player player) {
+	public void setPlayer(Player player) {
 		this.player = player;
 	}
 
-
- public void arbitrate(Player player){
-		
+	public void arbitrate(Player player){
+	 int randomNum = (int)(Math.random() * 11); // 0 to 10
+	 	if (randomNum >= 0 && randomNum <= 5) {
+			return;
+		}
+		if (randomNum > 5 && randomNum <= 8) {
+			if (player.hasYellowCard()) {
+				player.setRedCard(true);
+			} else { player.setYellowCard(true);}
+			
+		} else {player.setRedCard(true);}
 	}
-	
-	
+
 }
